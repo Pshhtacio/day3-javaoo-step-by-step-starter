1973296d41c78ad556c2abca68dc53b22005e013
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-16 22:38:49 +0800

    feat: Added leader field and optimized introduce method

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -6,16 +6,25 @@ public class Student extends Person {
 
     private Klass klass = null;
 
+    private boolean isLeader = false;
+
     private final String introduction = String.format("%s I am a student.", super.introduce());
 
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
+    public void setLeader(boolean isLeader) {
+        this.isLeader = isLeader;
+    }
+
     public String introduce() {
-        return Objects.nonNull(this.klass)
-                ? String.format("%s I am in class 1.", introduction)
-                : introduction;
+        if (Objects.nonNull(this.klass)) {
+            return isLeader
+                    ? String.format("%s I am the leader of class %d.", introduction, this.klass.getNumber())
+                    : String.format("%s I am in class 1.", introduction);
+        }
+        return introduction;
     }
 
     public void join(Klass klass) {
