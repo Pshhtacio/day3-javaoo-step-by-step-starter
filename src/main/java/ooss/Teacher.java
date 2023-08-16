c36e6c5090bffef4b9002abf5760df8d73f3ec25
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-17 00:08:06 +0800

    feat: Added getter method for multiple klass and refactor klass attribute

--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -2,31 +2,37 @@ package ooss;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Teacher extends Person {
 
     private final String introduction = String.format("%s I am a teacher.", super.introduce());
-    private List<Klass> klass = new ArrayList<>();
+    private List<Integer> klass = new ArrayList<Integer>();
+
     public Teacher(int id, String name, int age) {
         super(id, name, age);
     }
 
+    public String getKlasses(){
+        return this.klass.stream()
+                .map(klass -> Integer.toString(klass))
+                .collect(Collectors.joining(", "));
+    }
+
     public String introduce() {
         return !this.klass.isEmpty()
-                ? String.format("%s I teach Class %s.", introduction, klass.stream()
-                    .map(klass -> Integer.toString(klass.getNumber()))
-                    .collect(Collectors.joining(", ")))
-                : introduction;
+                ? String.format("%s I teach Class %s.", this.introduction, this.klass.stream()
+                .map(klass -> Integer.toString(klass))
+                .collect(Collectors.joining(", ")))
+                : this.introduction;
     }
 
     public void assignTo(Klass klass) {
-        this.klass.add(klass);
+        this.klass.add(klass.getNumber());
     }
 
     public boolean belongsTo(Klass klass) {
-        return this.klass.contains(klass);
+        return this.klass.contains(klass.getNumber());
     }
 
     public boolean isTeaching(Student student) {
