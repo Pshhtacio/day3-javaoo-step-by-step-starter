837786e4ffc9911f7eda39a33253a14dae589f69
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-17 00:06:16 +0800

    feat: Added getter method for name

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -19,6 +19,10 @@ public class Person {
         return id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public String introduce() {
         return String.format("My name is %s. I am %d years old.", this.name, this.age);
     }
