29d18b6cb41f387b18151b99db044765334d51a3
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-16 21:53:19 +0800

    feat: Add getKlass method and refactor code

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -6,13 +6,16 @@ public class Student extends Person {
 
     private Klass klass = null;
 
-    private String introduction = String.format("%s I am a student.", super.introduce());
+    private final String introduction = String.format("%s I am a student.", super.introduce());
+
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
     public String introduce() {
-        return Objects.nonNull(this.klass)? String.format("%s I am in class 1.", introduction) : introduction;
+        return Objects.nonNull(this.klass)
+                ? String.format("%s I am in class 1.", introduction)
+                : introduction;
     }
 
     public void join(Klass klass) {
@@ -21,6 +24,10 @@ public class Student extends Person {
 
     public boolean isIn(Klass klass) {
         return this.klass == klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
     }
 
 }
