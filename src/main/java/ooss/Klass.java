74b702a85a0ee03287c8e1a4f501555a7c9ac179
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-16 22:37:14 +0800

    feat: Added attribute leader and implement new method assignLeader, isInSameClass, and isLeader for Klass, removed unused imports

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -1,10 +1,9 @@
 package ooss;
 
-import java.util.Objects;
-
 public class Klass {
 
     private final int number;
+    private int leader = 0;
 
     public Klass(int number) {
         this.number = number;
@@ -20,5 +19,22 @@ public class Klass {
         if (!(o instanceof Klass)) return false;
         Klass klass = (Klass) o;
         return number == klass.number;
+    }
+
+    public void assignLeader(Student student) {
+        if (isInSameClass(student)) {
+            this.leader = student.getId();
+            student.setLeader(true);
+        } else {
+            System.out.println("It is not one of us.");
+        }
+    }
+
+    private boolean isInSameClass(Student student) {
+        return student.getKlass() != null && student.getKlass().equals(this);
+    }
+
+    public boolean isLeader(Student student) {
+        return this.leader == student.getId();
     }
 }
