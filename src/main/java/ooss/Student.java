95870d2a9d38097af8deba0993c26e8836af8833
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-16 20:25:39 +0800

    feat: Implement should_return_message_with_name_and_age_when_introduce_given_student add method introduce to Student and Teacher Class

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,8 +1,11 @@
 package ooss;
 
 public class Student extends Person {
-
     public Student(int id, String name, int age) {
         super(id, name, age);
+    }
+
+    public String introduce() {
+        return String.format("%s I am a student.", super.introduce());
     }
 }
--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -1,12 +1,11 @@
 package ooss;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Teacher extends Person {
     public Teacher(int id, String name, int age) {
         super(id, name, age);
+    }
+
+    public String introduce() {
+        return String.format("%s I am a teacher.", super.introduce());
     }
 }
