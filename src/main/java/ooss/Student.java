f1bc1e7b97f360cf1f205b25d66887e82637817d
Mhark Joseph <mharkjgalang@gmail.com>
2023-08-16 21:11:06 +0800

    feat: Add attribute "klass" and behaviour "join" and "isin" for Student object

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,11 +1,26 @@
 package ooss;
 
+import java.util.Objects;
+
 public class Student extends Person {
+
+    private Klass klass = null;
+
+    private String introduction = String.format("%s I am a student.", super.introduce());
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
     public String introduce() {
-        return String.format("%s I am a student.", super.introduce());
+        return Objects.nonNull(this.klass)? String.format("%s I am in class 1.", introduction) : introduction;
     }
+
+    public void join(Klass klass) {
+        this.klass = klass;
+    }
+
+    public boolean isIn(Klass klass) {
+        return this.klass == klass;
+    }
+
 }
